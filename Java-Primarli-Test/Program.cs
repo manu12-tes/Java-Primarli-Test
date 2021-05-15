350b9d790e0bcd720efcdc8fa732ea70b7406a65
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-14 23:29:55 -0500

    Agregar archivos de proyecto.

---
+++ Java-Primarli-Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Java_Primarli_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EScribe un numero");
+            int n = Int32.Parse(Console.ReadLine());
+
+            if (n%1==0 && n % n == 0) { Console.WriteLine("Primo"); }
+            else { Console.WriteLine("No es Primo"); }
+        }
+    }
+}
